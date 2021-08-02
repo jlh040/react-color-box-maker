3c924f7f0657a113faed6f15ca67e708e4eefad3
Jeff Hall <jeff.leon.hall@gmail.com>
2021-08-02 14:13:17 -0700

    added smoke and snapshot tests for the NewBoxForm

---
+++ src/NewBoxForm.test.js
@@ -0,0 +1,11 @@
+import { render } from '@testing-library/react';
+import NewBoxForm from './NewBoxForm';
+
+it('renders without crashing', () => {
+    render(<NewBoxForm />);
+});
+
+it('matches the snapshot', () => {
+    const { asFragment } = render(<NewBoxForm />);
+    expect(asFragment()).toMatchSnapshot();
+})
