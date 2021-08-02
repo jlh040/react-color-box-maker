4207611657d6b414b7f1216ee7b53b20d7e97f91
Jeff Hall <jeff.leon.hall@gmail.com>
2021-08-02 12:10:02 -0700

    created intial state for the boxes

--- src/BoxList.js
+++ src/BoxList.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 
 const BoxList = () => {
-
+  const [boxes, setBoxes] = useState([{
+    backgrouncColor: 'blue',
+    width: '30px',
+    height: '30px'
+  }]);
 };
 
 export default BoxList;
