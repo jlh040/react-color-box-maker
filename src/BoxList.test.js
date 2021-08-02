4020f01ee56802c1049f57f8d8f3de26284fc8af
Jeff Hall <jeff.leon.hall@gmail.com>
2021-08-02 14:20:11 -0700

    wrote smoke and snapshot tests for the BoxList component

--- src/BoxList.test.js
+++ src/BoxList.test.js
@@ -1,2 +1,11 @@
 import { render, fireEvent } from '@testing-library/react';
 import BoxList from './BoxList';
+
+it('renders without crashing', () => {
+    render(<BoxList />);
+});
+
+it('matches the snapshot', () => {
+    const { asFragment } = render(<BoxList />);
+    expect(asFragment()).toMatchSnapshot();
+})
